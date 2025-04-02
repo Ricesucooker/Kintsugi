1d82deb3fe4aafe83471f983177e7519ab2b095e
Matcha  MacBook.air <pockynekokun@gmail.com>
2025-04-02 21:41:31 +0100

    update folder str
    exit
    q
    :q

--- main.py
+++
@@ -1,9 +0,0 @@
-from google import genai
-
-client= genai.CLient(" ") ##api key goes here 
-
-response = client.models.generate_content(
-    model="gemini-2.0-flash", contents=" Explain how AI works in a few words"
-)
-
-print(response.txt)
