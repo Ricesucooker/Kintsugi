71c70d910c21cd25bc1f78d2227f23d9fa166f0c
Matcha  MacBook.air <pockynekokun@gmail.com>
2025-10-26 15:40:12 +0000

    rendering data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,34 @@
 import { useQuery, useQueryClient } from '@tanstack/react-query'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 export const BaseURI="http://localhost:8000/api/v1" 
+
 
 function App() {
 
   const {data} = useQuery({
 
     queryKey:[ 'mynotes' ],
-    queryFn: getMynotes})
+    queryFn: getMynotes});
+
+
+    const noteList = data || [];
+
 
   return (
     <>
     <h2>Hello there</h2>
     <div>{JSON.stringify(data)}</div>
+
+    <h3>Data below</h3>
+    <ul>
+      {noteList.map((data:any) =>{
+        return(
+          <li className='' key={data.noteid}><h2 className='text-lg'>{data.notetitle}</h2><p>{data.notebody}</p></li>
+        )
+      })}
+    </ul>
+
     </>
   )
 }
